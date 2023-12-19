c22188872207567d61190e1b24ba17d657144a8f
Raghavendra-J-R <121816454+Raghavendra-J-R@users.noreply.github.com>
2023-12-19 23:03:06 +0530

    Update Search.jsx

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -8,7 +8,7 @@ const Search = ({ onChange, onSearch }) => {
                     <div className="flex">
 
                         <input type="text" className="w-full max-w-[160px] bg-white pl-2 text-base font-semibold outline-0" placeholder="" id="" onChange={onChange} />
-                        <input type="button" value="Search" className="bg-gray-500 p-2 rounded-tr-lg rounded-br-lg text-white font-semibold hover:bg-gray-800 transition-colors" />
+                        <input type="button" value="Search" className="bg-gray-500 p-2 rounded-tr-lg rounded-br-lg text-white font-semibold hover:bg-gray-800 transition-colors" onClick={onSearch} />
                     </div>
                 </div>
             </div>
