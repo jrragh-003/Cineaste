3ef8f222730a6d804410e06100d8bfc3f3b8632e
Raghavendra <121816454+Raghavendra-J-R@users.noreply.github.com>
2024-04-15 18:20:33 +0530

    updated paymentController.js

--- backend/controller/paymentController.js
+++ backend/controller/paymentController.js
@@ -1,34 +1,62 @@
-const shortid = require('shortid')
-const Razorpay = require('razorpay')
+const shortid = require("shortid");
+const Razorpay = require("razorpay");
+const nodemailer = require("nodemailer");
 
 const razorpay = new Razorpay({
-	key_id: 'rzp_test_w5gM0MlnOtBwFl',
-	key_secret: 'u71SDuXyKWqpRa0cW5nU6paN'
+  key_id: process.env.key_id,
+  key_secret: process.env.key_secret,
 });
 const makePayment = async (req, res) => {
-	const payment_capture = 1
-	const amount = 150
-	const currency = 'INR'
+  const payment_capture = 1;
+  const amount = 150;
+  const currency = "INR";
 
-	const options = {
-		amount: amount * 100,
-		currency,
-		receipt: shortid.generate(),
-		payment_capture
-	}
+  const options = {
+    amount: amount * 100,
+    currency,
+    receipt: shortid.generate(),
+    payment_capture,
+  };
 
-	try {
-		const response = await razorpay.orders.create(options);
-		console.log(response);
-		res.json({
-			id: response.id,
-			currency: response.currency,
-			amount: response.amount,
-		});
-	} catch (error) {
-		console.log(error);
-		res.status(500).json({ error: 'Internal Server Error' });
-	}
-}
-	
-module.exports = {makePayment}
+  try {
+    const response = await razorpay.orders.create(options);
+    console.log(response);
+    sendRegistrationEmail(req.body.mail);
+    res.json({
+      id: response.id,
+      currency: response.currency,
+      amount: response.amount,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+const sendRegistrationEmail = async (toEmail) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.user,
+        pass: process.env.pass,
+      },
+    });
+
+    // send mail with defined transport object
+    const info = await transporter.sendMail({
+      from: '"Cineaste" <jrragh@gmail.com>', // sender address
+      to: toEmail,
+      subject: "Hello", // Subject line
+      text: "Hello world?", // plain text body
+      html: "<b>Hello world?</b>", // html body
+    });
+
+    console.log("Message sent: %s", info.messageId);
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  } catch (error) {
+    console.error("Error sending email:", error);
+  }
+};
+
+module.exports = { makePayment };
