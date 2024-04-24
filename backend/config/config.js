644f5b8223537ba5e766cdbb4adf2554c6faa868
Raghavendra <121816454+Raghavendra-J-R@users.noreply.github.com>
2024-04-24 20:28:14 +0530

    Updated config.js

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,10 +1,29 @@
 // config.js
+// require("dotenv").config();
+
+// const mongoose = require("mongoose");
+
+// mongoose.connect(process.env.DATABASE_URL);
+
+// const db = mongoose.connection;
+// db.on("error", console.error.bind(console, "MongoDB connection error:"));
+// db.once("open", () => {
+//   console.log("Connected to MongoDB");
+// });
+
+// mongoose.set("strictQuery", false);
+
+// module.exports = {
+//   mongoose,
+// };
+
+
+// config.js
 require("dotenv").config();
 
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.DATABASE_URL);
-
+mongoose.connect("mongodb://mongo-db/raghsData");
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 db.once("open", () => {
