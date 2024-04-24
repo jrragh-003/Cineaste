386a21e9196a8205b7817c42bf0be28084492803
Raghavendra <121816454+Raghavendra-J-R@users.noreply.github.com>
2024-04-24 20:30:15 +0530

    Updated vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,32 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react-swc';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:4000',
+//         changeOrigin: true,
+        
+//       },
+//     },
+//   },
+// });
+
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:4000',
+      "/api": {
+        target: "http://node-api:5000",
         changeOrigin: true,
-        
       },
     },
   },
