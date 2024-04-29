6ab379f6bff22b37c9e895bb9a97f6b6a555ecd1
Raghavendra <121816454+Raghavendra-J-R@users.noreply.github.com>
2024-04-30 00:55:26 +0530

    updated userController.js

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,57 +1,65 @@
 const userdb = require("../model/users");
-const bcrypt = require("bcrypt")
+const auth = require("../middleware/auth");
 const createUser = async (req, res) => {
-    try {
-      const salt = await bcrypt.genSalt(10);
-      const hashedPass = await bcrypt.hash(req.body.password, salt);
-   
+  try {
+    const hashedPass = await auth.hashPassword(req.body.password);
 
-      const newUser = new userdb({
-        name: req.body.name,
-        phoneNumber: req.body.phoneNumber,
-        mail: req.body.email,
-        password: hashedPass,
-      });
-      const existUser = await userdb.findOne({mail:req.body.email});
+    const newUser = new userdb({
+      name: req.body.name,
+      phoneNumber: req.body.phoneNumber,
+      mail: req.body.email,
+      password: hashedPass,
+    });
+    const existUser = await userdb.findOne({ mail: req.body.email });
 
-      if (existUser) {        
-        return res.status(422).json({ message: 'User with the same email already exists' });
+    if (existUser) {
+      return res
+        .status(422)
+        .json({ message: "User with the same email already exists" });
     }
-     
-  
-      const user = await newUser.save();
-      
-      const { password, ...others } = user._doc;
-      res.status(200).json(others);
-    //   console.log("Hey yo done finally")
-    } catch (error) {
-      console.error('Error creating user:', error);
-      
-      res.status(500).json({ message: 'Error creating User' });
+
+    const user = await newUser.save();
+
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
+  } catch (error) {
+    console.error("Error creating user:", error);
+
+    res.status(500).json({ message: "Error creating User" });
+  }
+};
+
+const createLogin = async (req, res) => {
+  try {
+    const user = await userdb.findOne({ mail: req.body.email });
+    if (!user) {
+      return res.status(400).json("Wrong credentials!");
     }
-  };
-  
-  const createLogin = async (req, res) => {
-    try {
-      const user = await userdb.findOne({ mail:req.body.email });
-      if (!user) {
-        return res.status(400).json('Wrong credentials!');
-      }
-  
-      const validated = await bcrypt.compare(req.body.password, user.password);
-      if (!validated) {
-        return res.status(400).json('Wrong credentials!');
-      }
-  
-      const { password, ...others } = user._doc;
-      console.log(others)
-      return res.status(200).json(others);
-      
-    } catch (err) {
-      console.error('Error logging in:', err);
-     return  res.status(500).json(err);
+
+    const validated = await auth.comparePasswords(
+      req.body.password,
+      user.password
+    );
+    if (!validated) {
+      return res.status(400).json("Wrong credentials!");
     }
-  };
-  
-  module.exports = { createUser, createLogin };
-  
+    const token = auth.createJWT(user);
+    const options = {
+      expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+      httpOnly: true,
+    };
+    // console.log(token);
+    res.cookie("token", token, options).status(200).json({
+      sucess: "true",
+      token,
+      user,
+      message: "User Login Successfull",
+    });
+    // return res.status(200).json({ msg: "Successfully Logged in" });
+  } catch (err) {
+    console.error("Error logging in:", err);
+    return res.status(500).json(err);
+  }
+};
+
+module.exports = { createUser, createLogin };
