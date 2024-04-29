567605f440385cdb8e3bd49d133279c5e8939451
Raghavendra <121816454+Raghavendra-J-R@users.noreply.github.com>
2024-04-30 00:57:10 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,24 @@
-
 const express = require("express");
 const cors = require("cors");
-const bodyParser = require('body-parser'); // Add this line
-const config = require('./config/config')
+const bodyParser = require("body-parser"); // Add this line
+const cookieParser = require("cookie-parser");
+const config = require("./config/config");
 
 const app = express();
 
 // Middlewares
 app.use(cors());
+app.use(cookieParser());
 app.use(bodyParser.json()); // Add this line
 
-const reviewRoutes = require('./routes/reviewRoute')
-app.use('/api',reviewRoutes)
+const reviewRoutes = require("./routes/reviewRoute");
+app.use("/api", reviewRoutes);
 
-const userRoutes = require('./routes/userRoute');
-app.use('/api',userRoutes);
+const userRoutes = require("./routes/userRoute");
+app.use("/api", userRoutes);
 
+const paymentRoute = require("./routes/paymentRoute");
+app.use("/api", paymentRoute);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
