61806d4768f73815b7ff6fea7ea7b640c90ac476
Raghavendra <121816454+Raghavendra-J-R@users.noreply.github.com>
2024-04-30 00:56:06 +0530

    updated reviewRoute.js

--- backend/routes/reviewRoute.js
+++ backend/routes/reviewRoute.js
@@ -1,12 +1,19 @@
-const express = require('express');
+const express = require("express");
+const auth = require("../middleware/auth");
 const router = express.Router();
-const reviewController = require('../controller/reviewController')
+const reviewController = require("../controller/reviewController");
 
+router.post("/writeReview", auth.protect, reviewController.createReviews);
+router.get("/getReviews/:title", auth.protect, reviewController.getReviews);
+router.patch(
+  "/editReview/:title/:id",
+  auth.protect,
+  reviewController.updateReviews
+);
+router.delete(
+  "/deleteReview/:title/:id",
+  auth.protect,
+  reviewController.deleteReviews
+);
 
-router.post('/writeReview',reviewController.createReviews);
-router.get('/getReviews/:title',reviewController.getReviews)
-router.patch('/editReview/:title/:id', reviewController.updateReviews);
-router.delete('/deleteReview/:title/:id',reviewController.deleteReviews)
-
-
-module.exports = router
+module.exports = router;
